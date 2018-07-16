e95d5bcf9294a827df9d4dc9bd7d68361d6b544d
Tiffany <tiffanyjdow@gmail.com>
2018-07-16 21:50:08 +0000

    Added files

--- filters.rb
+++ filters.rb
@@ -1,16 +1,70 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+#detect method in ruby --> returns 1st item in collection for which the
+#block returns TRUE. Returns nil if it doesn't find a match.
+#select/find_all will iterate until the end of the input list is reached
+#returns all item in an array for which the block returned true
+
 def find(id)
-  # Your code Here
+  @candidates.detect { |candidate| candidate[:id] == id }
 end
+
 
 def experienced?(candidate)
-  # Your code Here
+  candidate[:years_of_experience] >= 2
 end
+
 
 def qualified_candidates(candidates)
-  # Your code Here
+  candidates.select { |candidate| experienced?(candidate) &&
+    github_points_over_100?(candidate) &&
+    knows_ruby_or_python(candidate) &&
+    applied_less_15_days?(candidate) &&
+    over_17?(candidate) }
 end
 
+
 # More methods will go below
+
+def github_points_100?(candidate)
+  candidate[:github_points] >= 100
+end
+
+
+def knows_ruby_or_python(candidate)
+  candidate[:languages].include?('Python') ||
+  candidate[:languages].include?('Ruby')
+end
+
+
+def applied_recent?(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+
+def of_age?(candidate)
+  candidate[:age] > 17
+end
+
+
+def ordered_by_qualifications(candidates)
+  @candidates.sort_by { |experience| experience[:years_of_experience] }.reverse
+    if [:years_of_experience].eql? [:years_of_experience]
+      candidates.sort_by { |github_points| git_points[:github_points] }.reverse
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.rb
+++ main.rb
@@ -4,9 +4,55 @@
 require 'pry'
 require './candidates'
 require './filters'
+require './candidate'
 
 ## Your test code can go here
 
-# binding.pry
+ binding.pry
 
-pp @candidates
+ pp @candidates
+
+# class CandidateFinder
+#   #  `candidates` should be an array of Candidate objects here
+#   def initialize(candidates)
+#     @candidates = candidates
+#   end
+
+  # Class method which takes an array of hashes and initializes Candidate objects from the hashes
+  # def self.init_from_hash(candidates)
+  #   #Map hash objects to Candidate objects
+  #   candidatesObjects = candidates.map { |c| Candidate.new(c) }
+  #   #Call the initialize function with our new array of Candidates
+  #   self.new(candidatesObjects)
+  # end
+
+#   def find(id)
+#     @candidates.select { |c| c.id == id }.first
+#   end
+
+#   def qualified
+#     @candidates.select { |c| c.qualified? }
+#   end
+# end
+
+# cf = CandidateFinder.init_from_hash(@candidates)
+# pp cf.qualified
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
