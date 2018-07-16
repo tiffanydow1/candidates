2f27eea3f40f7328032d01c0c87fda13a4ae910e
Tiffany <tiffanyjdow@gmail.com>
2018-07-16 21:55:36 +0000

    commited out candidate.rb file

--- candidate.rb
+++ candidate.rb
@@ -1,43 +1,43 @@
-class Candidate
-  attr_reader :id
+# class Candidate
+#   attr_reader :id
 
-  def initialize(hash)
-    @id = hash[:id]
-    @years_of_experience = hash[:years_of_experience]
-    @github_points = hash[:github_points]
-    @languages = hash[:languages]
-    @date_applied = hash[:date_applied]
-    @age = hash[:age]
-  end
+#   def initialize(hash)
+#     @id = hash[:id]
+#     @years_of_experience = hash[:years_of_experience]
+#     @github_points = hash[:github_points]
+#     @languages = hash[:languages]
+#     @date_applied = hash[:date_applied]
+#     @age = hash[:age]
+#   end
 
-  def qualified?
-    experienced? &&
-    has_github_points? &&
-    knows_languages? &&
-    recent_application? &&
-    over_age_of_majority?
-  end
+#   def qualified?
+#     experienced? &&
+#     has_github_points? &&
+#     knows_languages? &&
+#     recent_application? &&
+#     over_age_of_majority?
+#   end
 
-  def experienced?
-    @years_of_experience >= 2
-  end
+#   def experienced?
+#     @years_of_experience >= 2
+#   end
 
-  def has_github_points?
-    @github_points >= 100
-  end
+#   def has_github_points?
+#     @github_points >= 100
+#   end
 
-  def knows_languages?
-    @languages.any? { |l| l == 'Ruby' || l == 'Python' }
-  end
+#   def knows_languages?
+#     @languages.any? { |l| l == 'Ruby' || l == 'Python' }
+#   end
 
-  def recent_application?
-    @date_applied >= 15.days.ago
-  end
+#   def recent_application?
+#     @date_applied >= 15.days.ago
+#   end
 
-  def over_age_of_majority?
-    @age > 17
-  end
-end
+#   def over_age_of_majority?
+#     @age > 17
+#   end
+# end
 
 
 
